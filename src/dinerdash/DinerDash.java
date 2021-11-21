cc0d7c5affeb420e5e2f2c1a98b062324adc6965
Sebasflox <sebas33115@gmail.com>
2021-11-20 21:57:50 -0600

    Update DinerDash.java

--- src/dinerdash/DinerDash.java
+++ src/dinerdash/DinerDash.java
@@ -4,8 +4,9 @@ package dinerdash;
 public class DinerDash {
 
     public static void main(String[] args) {
-        
+
         System.out.println("Prueba repositorio GitHub");
+        System.out.println("Pura vida");
         
     }
     
