0c6f355ef2a84775f213b9c1043d74e5f0c2ae34
Sebasflox <sebas33115@gmail.com>
2021-11-26 00:01:35 -0600

    Update Nodo_FilaEntrada.java

--- src/dinerdash/Nodo_FilaEntrada.java
+++ src/dinerdash/Nodo_FilaEntrada.java
@@ -1,21 +1,9 @@
 
 package dinerdash;
-import java.util.Random;
 
 public class Nodo_FilaEntrada {
     public Nodo_FilaEntrada siguiente;
     public int fila_de_la_entrada;
-    
-    Random random = new Random();
-    
-    
-    public void Nodo_FilaEntrada(){ //Buscar la menera de temporizar la entrada de clientes
-        Cola_FilaEntrada app = new Cola_FilaEntrada();
-    
-        int indice_random = random.nextInt(6);
-        app.enqueue(indice_random);
-        
-    }
-    
+       
 }
 
