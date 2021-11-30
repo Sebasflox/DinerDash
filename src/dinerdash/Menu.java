62d64d550f2680553e146c241ccd546f86681f92
Sebasflox <sebas33115@gmail.com>
2021-11-30 00:24:38 -0600

    Create Menu.java

---
+++ src/dinerdash/Menu.java
@@ -0,0 +1,20 @@
+
+package dinerdash;
+
+import java.util.Random;
+
+public class Menu {
+    
+    Random random = new Random();
+    private int [] Menu = {1, 2, 3};
+    
+    public void Menu_comida(){
+        Cola_FilaEntrada app = new Cola_FilaEntrada();
+        
+        //for(int x=0; x<app.dequeue(); x++){
+        //    int indice_random = random.nextInt(Menu.length);
+        //}
+        
+    }
+       
+}
