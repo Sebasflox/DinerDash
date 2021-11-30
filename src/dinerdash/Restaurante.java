f9e7f4ba3f65246b16a58f26832ddab549045f6b
Roy Nájar <roy_najar@hotmail.com>
2021-11-30 01:03:44 -0600

    Update Restaurante.java

--- src/dinerdash/Restaurante.java
+++ src/dinerdash/Restaurante.java
@@ -1,29 +1,21 @@
-
 package dinerdash;
 
-
 public class Restaurante {
-   
+    Cola_FilaEntrada app = new Cola_FilaEntrada();
     private int Mesas [][] = new int [15][6];
     
     public void Mesas(){
-        
-        Cola_FilaEntrada app = new Cola_FilaEntrada();
-        BusinessLogic sacar = new BusinessLogic();
-
-    // enter array elements.
-    for(int x = 0; x < Mesas.length; x++) {
-      for (int y = 0; y < Mesas[x].length; y++) {
-        Mesas[x][y] = app.Valor;
-      }
+        for(int x = 0; x < Mesas.length; x++) {
+            for (int y = 0; y < Mesas[x].length; y++) {
+                Mesas[x][y] = app.Valor;//estamos teniendo problemas con éste valor, no retorna el valor del primero de la cola
+                //hola
+            }
+        }
+        for (int x = 0; x < Mesas.length; x++) {
+            for (int y = 0; y < Mesas[x].length; y++) {
+                System.out.print(Mesas[x][y] + "  ");
+            }
+            System.out.println();
+        }
     }
-
-    for (int x = 0; x < Mesas.length; x++) {
-      for (int y = 0; y < Mesas[x].length; y++) {
-        System.out.print(Mesas[x][y] + "  ");
-      }
-      System.out.println();
-    }
-  }
-    
 }
