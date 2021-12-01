7f117e0ec78ebd489b69b9a8a27c5a99c97aec02
Sebasflox <sebas33115@gmail.com>
2021-11-30 23:21:28 -0600

    Update BusinessLogic.java

--- src/dinerdash/BusinessLogic.java
+++ src/dinerdash/BusinessLogic.java
@@ -8,14 +8,11 @@ public class BusinessLogic {
     
         public void Random(){
 
-        do {
             int indice_random = generador.nextInt(6); //Se logro quitar el cero, estar al tanto de esto
             app.enqueue(indice_random + 1);
-        }
-        while (app.contar() < 5);
-        app.imprimir(); //este imprime el valor del primero de la cola
-        app.contar(); //este imprime el contador de los datos en la cola
-        
+            System.out.println(indice_random);
+            
+            System.out.println();
     }
     
 }
